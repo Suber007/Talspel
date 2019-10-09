310b26ae34365afc8b0900ca5744567b07a547f0
Kemal Cikota <susa02001@utb.vaxjo.se>
2019-10-09 15:59:00 +0200

    Lagt till projekt

---
+++ Talspel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talspel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int gen = rand.Next(1, 101);
+
+            Console.WriteLine("Hej, Skriv Ett Tal Mellan 1 och 100 : ");
+            int.tal;
+
+            do
+            {
+                tal.int()
+
+
+
+
+            }
+
+
+              
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+            
+        }
+    }
+}
